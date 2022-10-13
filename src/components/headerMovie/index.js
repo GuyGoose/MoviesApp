b9f14d05c5ad4caf7db9565e7baff27d960f5ee4
GuyGoose <antokeeffe@outlook.ie>
2022-10-13 15:09:27 +0100

    Add to favourites logic completed; Dummy favourites page.

--- src/components/headerMovie/index.js
+++ src/components/headerMovie/index.js
@@ -5,10 +5,16 @@ import Paper from "@mui/material/Paper";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import HomeIcon from "@mui/icons-material/Home";
+import Avatar from "@mui/material/Avatar";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import CardHeader from "@mui/material/CardHeader";
+
 
 const MovieHeader = (props) => {
   const movie = props.movie;
-
+  const movies = JSON.parse(localStorage.getItem("favourites"));
+  const ids = movies.map((m) => m.id)
+  
   return (
     <Paper 
         component="div" 
@@ -23,6 +29,15 @@ const MovieHeader = (props) => {
       <IconButton aria-label="go back">
         <ArrowBackIcon color="primary" fontSize="large" />
       </IconButton>
+
+      {
+        ids.includes(props.movie.id) ? (
+          <Avatar sx={{ backgroundColor: 'red' }}>
+            <FavoriteIcon />
+          </Avatar>
+        ) : null
+      }
+      
 
       <Typography variant="h4" component="h3">
         {movie.title}
